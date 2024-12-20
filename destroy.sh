19bfb5d8382f92b1824e9aeeb87141a0eb2c2d30
Nicolas Theau <theaun91@gmail.com>
2024-12-20 09:46:10 +0000

    fix destroy.sh .azure qui n'était pas au bon endroit

--- destroy.sh
+++ destroy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker container run -it --rm -v $(pwd)/Devops/Terraform:/workspace -v ~/.azure:/root/.azure -w /workspace terraform-azure-cli terraform destroy -auto-approve
+docker container run -it --rm -v $(pwd)/Devops/Terraform:/workspace -v $(pwd)/Devops/Terraform/.azure:/root/.azure -w /workspace terraform-azure-cli terraform destroy -auto-approve
